cb7c3876f41c507259bc041f23c0c56a3891dcda
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-20 21:20:57 -0300

    20/08

---
+++ untitled/src/main/java/org/example/Veiculos/Cliente.java
@@ -0,0 +1,28 @@
+package org.example.Veiculos;
+
+public class Cliente {
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+
+    public Cliente(String nome, int idade, String cpf, String endereco, String telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cpf='" + cpf + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
--- untitled/src/main/java/org/example/Veiculos/DadosVeiculos.java
+++ untitled/src/main/java/org/example/Veiculos/DadosVeiculos.java
@@ -3,7 +3,10 @@ package org.example.Veiculos;
 public class DadosVeiculos {
     public static void main(String[] args) {
         Veiculos veiculo1 = new Veiculos("SDK87W", "Branco", 4, "55L", 487, 10);
+        Cliente cliente1 = new Cliente("Yan", 24, "999999999","Estrada da liberdade","71985242246");
 
         System.out.println("Dados do 1º veiculo: " + veiculo1.toString());
+        System.out.println("Dados do 1º Cliente: " + cliente1.toString());
+
     }
 }
